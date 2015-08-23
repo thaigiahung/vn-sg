2f442b541a4c3261ac805c8baa882e82e360fcea
Gia Hung Thai <hung.tg89@gmail.com>
2015-08-23 16:29:15 +0700

    Add view Product List

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -5,7 +5,32 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+
 module.exports = {
-	
+	view: function(req, res) {
+    Product.find().exec(function (err, products) {
+      if(err || !products) {
+        return res.view('index', {data: []});
+      }
+      else {
+        var data = [];
+        async.eachSeries(products, function iterator(product, callback) {
+          ProductImages.find({product: product.id}).exec(function (err, images) {
+            var prod = {
+              id: product.id,
+              name: product.name,
+              price: product.price,
+              images: images
+            }
+
+            data.push(prod);
+            callback(null, data);
+          });          
+        }, function done() {
+          res.view('index', {data: data});
+        });        
+      }
+    });
+  },
 };
 
