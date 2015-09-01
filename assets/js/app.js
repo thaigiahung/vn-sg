b1a02f0c954a098fb2efcd3ce06b1e488295c677
Gia Hung Thai <hung.tg89@gmail.com>
2015-09-01 10:49:29 +0700

    Add added item to Modal Cart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,27 @@
 function addCart (product, qty) {  
-  console.log(product);
-  console.log(qty);
+  $('#modalCartBody').html(
+                            '<tr>' +
+                              '<td>' +
+                                '<a href="/product/'+product.id+'">' +
+                                  '<img width="150" src="'+product.images[0].url+'" />' +
+                                '</a>' +                            
+                              '</td>' +
+                              '<td>' +
+                                '<a href="/product/'+product.id+'">' +
+                                  product.name +
+                                '</a>' +
+                              '</td>' +
+                              '<td>' +
+                                product.price +
+                              '</td>' +
+                              '<td>' +
+                               qty +
+                              '</td>' +
+                            '</tr>'
+                          );
   $('#modalCart').modal();
-}
+};
 
 $('.btn-checkout').click(function (event) {
   alert("B")
-})
+});
