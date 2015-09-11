df602b0863cc353f797fdcef2688cd51b816303e
Gia Hung Thai <hung.tg89@gmail.com>
2015-09-11 15:35:09 +0700

    Move all api key to new config file

---
+++ config/myconfig.js
@@ -0,0 +1,5 @@
+// http://stackoverflow.com/questions/18267706/create-config-variables-in-sails-js
+module.exports.myconfig = {
+  googleMap: 'AIzaSyDt1kN4lUOndAU2L3GDxS21PYFEySdLaZw',
+  mixpanel: '813fee0ef7061c06ffe1237de6b6997a',
+};
