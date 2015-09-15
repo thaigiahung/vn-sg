8e57ac346373c11bf1efafea36095b86cf86a871
Gia Hung Thai <hung.tg89@gmail.com>
2015-09-15 15:26:10 +0700

    Ignore myconfig.js

---
+++ config/myconfig.default.js
@@ -0,0 +1,13 @@
+// http://stackoverflow.com/questions/18267706/create-config-variables-in-sails-js
+module.exports.myconfig = {
+  siteURL: '',
+  googleMap: '',
+  mixpanel: '',
+  sendgrid: {
+    apiKey: '',
+    from: '',
+    fromName: '',
+    subject: '',
+    templateID: ''
+  }
+};
